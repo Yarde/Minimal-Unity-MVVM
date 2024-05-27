80c04c4b9a9bf1fb5db37c51c4606bf0eb73ec24
Dawid Kaluza <dawidkal66@gmail.com>
2024-05-27 21:04:27 +0200

    Add sample

--- Samples~/BindingSample/BindExample.cs
+++ Samples~/BindingSample/BindExample.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,18 +14,29 @@ namespace Yarde.UnityObservable.Samples_
     {
         [SerializeField] private TextMeshProUGUI _textMeshPro;
         [SerializeField] private Image _image;
-        
+        [SerializeField] private Button _button;
+
         private readonly IObservableValue<Color> _observableColor = new ObservableValue<Color>(Color.red);
-        private readonly IObservableValue<string> _observableString = new ObservableValue<string>("test");
-        private readonly IObservableValue<bool> _observableBool = new ObservableValue<bool>(true);
+        private readonly ObservableValue<string> _observableString = new ObservableValue<string>("test");
+        private readonly ObservableValue<bool> _observableBool = new ObservableValue<bool>(true);
+        
+        // to hide setter a wrapper to interface can be added
+        private IObservableValue<string> ObservableString => _observableString;
         
         private readonly DisposableList _disposables = new();
         
         private void Start()
         {
-            _textMeshPro.Bind(_observableString).AddTo(_disposables);
+            _button.onClick.AddListener(() => _observableBool.Value = !_observableBool.Value);
+            
+            _textMeshPro.Bind(ObservableString).AddTo(_disposables);
             _image.Bind(_observableColor).AddTo(_disposables);
             _image.gameObject.Bind(_observableBool).AddTo(_disposables);
+        }
+
+        private void Update()
+        {
+            _observableString.Value = Time.time.ToString(CultureInfo.InvariantCulture);
         }
 
         public void Dispose()
